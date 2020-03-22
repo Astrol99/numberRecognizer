c00776eea3c2b974730ecfd7a341cddb68b845f6
David <dngames2006@gmail.com>
2020-03-22 03:16:29 -0500

    Added basic neural netowrk

--- train.py
+++ train.py
@@ -6,3 +6,12 @@ import matplotlib.pyplot as plt
 MNIST = keras.datasets.mnist
 
 (train_images, train_labels), (test_images, test_labels) = MNIST.load_data()
+
+train_images = train_images / 255.0
+test_images  = test_images  / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])

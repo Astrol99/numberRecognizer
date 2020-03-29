f962933224dc5f539eaadd24f091ab2c86fc7e00
David <dngames2006@gmail.com>
2020-03-29 16:58:02 -0500

    Implemented number identification

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import pygame
 import numpy as np
 from tensorflow import keras
+from PIL import Image
 
-FPS = 144
 WHITE = (255, 255, 255)
 BLACK = (  0,   0,   0)
-
-def process_data():
-    pass
 
 # Get model
 model = keras.models.load_model('MNIST.h5')
@@ -16,19 +13,34 @@ print("[+] Loaded model -> MNIST.h5")
 pygame.init()
 
 screen = pygame.display.set_mode((644, 476))
-screen.fill(WHITE)
+screen.fill(BLACK)
 
-clock = pygame.time.Clock()
+pygame.display.set_caption("Number Recognizer")
+
+def process_data():
+    rect = pygame.Rect((0,0), (634, 466))
+    sub = screen.subsurface(rect)
+    pygame.image.save(sub, "screenshot.jpg")
+
+    img = Image.open("screenshot.jpg")
+    img = img.resize((28,28), Image.ANTIALIAS)
+
+    array = np.asarray(img)
+    array = array / 255.0
+    array = array[:, :, 0]
+    print(array.shape)
+    array = (np.expand_dims(array, 0))
+
+    return array
 
 mainloop = True
 continous_circle = False
 
 while mainloop:
-    ms = clock.tick(FPS)
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
     if continous_circle == True:
-        pygame.draw.circle(screen, BLACK, (mouse_x, mouse_y), 10)
+        pygame.draw.circle(screen, WHITE, (mouse_x, mouse_y), 10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,16 +50,15 @@ while mainloop:
         elif event.type == pygame.MOUSEBUTTONDOWN:
 
             if (mouse_x >= 10 and mouse_x <= 634) and (mouse_y >= 10 and mouse_y <= 466):
-                pygame.draw.circle(screen, BLACK, (mouse_x, mouse_y), 10)
+                pygame.draw.circle(screen, WHITE, (mouse_x, mouse_y), 10)
                 continous_circle = True
 
         elif event.type == pygame.MOUSEBUTTONUP:
             continous_circle = False
 
             # Run Model
-            predictions = model.predict(img)
+            predictions = model.predict(process_data())
             print(np.argmax(predictions[0]))
-
 
         elif event.type == pygame.KEYDOWN:
 
@@ -55,10 +66,7 @@ while mainloop:
                 mainloop = False
             
             if event.key == pygame.K_SPACE:
-                pygame.draw.rect(screen, WHITE, ((0, 0), (634, 466)))
-
-    text = f"FPS: {clock.get_fps()}"
-    pygame.display.set_caption(text)
+                pygame.draw.rect(screen, BLACK, ((0, 0), (634, 466)))
 
     pygame.display.update()
 
--- train.py
+++ train.py
@@ -19,7 +19,7 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=10)
+model.fit(train_images, train_labels, epochs=50)
 
 # Eval Accuracy
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
